d333de23cacd59c3c9e78f52a1682402a86ca5fc
Ligthning24 <revelessoto1@gmail.com>
2024-09-10 18:48:06 -0600

    10/09/2024 06:47pm

--- Practica1.java
+++ Practica1.java
@@ -4,4 +4,16 @@ public class Practica1 {
     public String direccion="";
     public String apellido="";
     
+
+    public class Pedidos{
+        public String articulos="";
+        public int num=0;
+    }
+    public class Pagos{
+        public int numpedido=0;
+        public String metodopago="";
+    }
+    public class Notificacion{
+        public String correo="";
+    }
 }
